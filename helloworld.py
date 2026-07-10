77a56a36f0d0e76e28daec8bc9326183c5926184
Kelvin <kelvinmiringu679@gmail.com>
2026-07-10 21:28:00 +0300

    my first python code

---
+++ helloworld.py
@@ -0,0 +1,5 @@
+name = input("what is your name?: ")
+age = int(input("How old are you?: "))
+height = float(input("How tall are you?: "))
+
+print("Your name is "+name+" ,you are "+str(age)+" years old and "+str(height)+"cm tall")
